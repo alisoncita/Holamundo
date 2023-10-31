a1be687564afa947456b9176a0a020199323ac46
alisoncita <149503983+alisoncita@users.noreply.github.com>
2023-10-31 09:52:12 -0500

    Add files via upload

---
+++ holamundo2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HOLA MUNDO</h1>
+    <img src="img/perros.png" alt="100" width="300">
+</body>
+</html>
